51c087e2f346bd9c25864ae61416bea4fb8d4d27
Alexandra Tang <alexandra.tang@appliedtechnology.se>
2019-07-17 09:34:24 +0200

    Added API key for SL Reseplanerare 3.1

--- the-homepage/src/components/Card.js
+++ the-homepage/src/components/Card.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { ContentCard, GreetingText } from "../styles/styles";
+const apiKey = process.env.REACT_APP_API_KEY;
 
 class Card extends Component {
+
   render() {
     return (
       <ContentCard>
