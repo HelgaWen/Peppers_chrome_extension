1724773253719ed1c49f7c1b7c46ff1780fd69e2
helga wennstedt <helga.wennstedt@appliedtechnology.se>
2019-07-18 16:19:49 +0200

    Setting and getting origin and destination from user input

--- the-homepage/src/components/Sl.js
+++ the-homepage/src/components/Sl.js
@@ -1,7 +1,60 @@
+/* global chrome */
 import React, { Component } from "react";
 import { ContentCard } from "../styles/general";
 import { SlInput, SlHeadline, InputContainer } from "../styles/slStyles";
 class SL extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasSlInfo : false,
+      SL: {
+        origin: 'Origin',
+        destination: 'Destination'
+      }
+    }
+    this.inputOrigin = React.createRef();
+    this.inputDestination = React.createRef();
+  }
+
+  componentDidMount() {
+    this.getInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage();
+  }
+
+  getInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage = () => {
+    chrome.storage.sync.get(["SL"], info => {
+      if (info.SL) {
+        this.setState(prevstate => ({
+          SL: { 
+              ...prevstate.SL,
+              origin: info.SL.origin,
+              destination: info.SL.destination
+            }
+          }))
+      } else {
+        this.setState({ hasSlInfo: false });
+      }
+    });
+  }
+
+  setInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage = () => {
+    chrome.storage.sync.set({ SL: {origin:this.state.SL.origin, destination:this.state.SL.destination }},  () => {
+      console.log("SL info is set to  " + this.state.SL.origin, this.state.SL.destination);
+    });
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      SL: { 
+          origin: this.inputOrigin.current.value,
+          destination: this.inputDestination.current.value
+        }
+      }, () => {
+        this.setInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage();
+      }
+    );
+  }
 
   render() {
     return (
@@ -11,9 +64,10 @@ class SL extends Component {
         </SlHeadline>
         <form onSubmit={this.onSubmit}>
           <InputContainer>
-            <SlInput placeholder="Origin" type="text" ref={this.title} />
+            <SlInput placeholder={this.state.SL.origin} type="text" ref={this.inputOrigin} />
             <h3> to </h3>
-            <SlInput placeholder="Destination" type="text" ref={this.description} />
+            <SlInput placeholder={this.state.SL.destination} type="text" ref={this.inputDestination} />
+          <input type='submit'></input>
           </InputContainer>
         </form>
         <h3>Leaves in: </h3>
