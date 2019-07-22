001a3616bcd312596a974709ac9e539e71d0fa20
zachary eriksson <zachary.eriksson@appliedtechnology.se>
2019-07-22 14:37:33 +0200

    began implemetation of SL fetch, encountered CORS problem

--- the-homepage/src/components/Sl.js
+++ the-homepage/src/components/Sl.js
@@ -2,15 +2,24 @@
 import React, { Component } from "react";
 import { ContentCard } from "../styles/general";
 import { Input, Headline, InputContainer } from "../styles/slStyles";
+
+const SLApiKey = '222e319bd06249f39ef7ad7319123f56';
+
 class SL extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      hasSlInfo : false,
+      hasSlInfo: false,
       SL: {
-        origin: 'Origin',
-        destination: 'Destination'
+        origin: {
+          name: 'Origin',
+          siteId: null
+        },
+        destination: {
+          name: 'Destination',
+          siteId: null
+        }
       }
     }
     this.inputOrigin = React.createRef();
@@ -19,17 +28,47 @@ class SL extends Component {
 
   componentDidMount() {
     this.getInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage();
+    this.fetchSiteId();
+  }
+
+  fetchSiteId = () => {
+    let newOriginId;
+    let newDestinatinId;
+
+    fetch(`https://api.sl.se/api2/typeahead.json?key=${SLApiKey}&searchstring=${this.state.SL.origin.name}&stationsonly=true&maxresults=1`, {
+
+    })
+      .then(result => result.json())
+      .then(data => {
+        console.log('YO!')
+        console.log(data)
+        // newOriginId = data.ResponseData.SiteId;
+        // fetch(`https://api.sl.se/api2/typeahead.json?key=${SLApiKey}&searchstring=${this.state.SL.destination.name}&stationsonly=true&maxresults=1`, {
+        //   mode: 'no-cors'
+        // })
+        //   .then(result => result.json())
+        //   .then(data => {
+
+
+        //     this.newDestinationId = data.ResponseData.SiteId
+        //     this.setState({ SL: { origin: { siteId: newOriginId }, destination: { siteId: newDestinatinId } } }, console.log('SL STATE IS: ', this.state.SL));
+        //   })
+      })
+  }
+
+  shouldWeFetch = () => {
+    return this.state.SL.origin !== 'Origin';
   }
 
   getInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage = () => {
     chrome.storage.sync.get(["SL"], info => {
       if (info.SL) {
         this.setState({
-          SL: { 
-              origin: info.SL.origin,
-              destination: info.SL.destination
-            }
-          })
+          SL: {
+            origin: info.SL.origin.name,
+            destination: info.SL.destination.name
+          }
+        })
       } else {
         this.setState({ hasSlInfo: false });
       }
@@ -37,23 +76,23 @@ class SL extends Component {
   }
 
   setInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage = () => {
-    chrome.storage.sync.set({ SL: {origin:this.state.SL.origin, destination:this.state.SL.destination }},  () => {
+    chrome.storage.sync.set({ SL: { origin: { name: this.state.SL.origin.name, siteId: this.state.SL.origin.siteId }, destination: { name: this.state.SL.destination.name, siteId: this.state.SL.destination.siteId } } }, () => {
       console.log("SL info is set to  " + this.state.SL.origin, this.state.SL.destination);
     });
   }
 
   onSubmit = event => {
     event.preventDefault();
-    const checkOrigin = this.inputOrigin.current.value === '' ? this.state.SL.origin : this.inputOrigin.current.value;
-    const checkDestination = this.inputDestination.current.value === '' ? this.state.SL.destination : this.inputDestination.current.value;
+    const checkOrigin = this.inputOrigin.current.value === '' ? this.state.SL.origin.name : this.inputOrigin.current.value;
+    const checkDestination = this.inputDestination.current.value === '' ? this.state.SL.destination.name : this.inputDestination.current.value;
     this.setState({
-      SL: { 
-          origin: checkOrigin,
-          destination: checkDestination
-        }
-      }, () => {
-        this.setInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage();
+      SL: {
+        origin: checkOrigin,
+        destination: checkDestination
       }
+    }, () => {
+      this.setInformationOfYourPersonallyChosenOriginAndDestinationFromYourChromeStorage();
+    }
     );
   }
 
@@ -68,7 +107,7 @@ class SL extends Component {
             <Input placeholder={this.state.SL.origin} type="text" ref={this.inputOrigin} />
             <h3> to </h3>
             <Input placeholder={this.state.SL.destination} type="text" ref={this.inputDestination} />
-          <input type='submit'></input>
+            <input type='submit'></input>
           </InputContainer>
         </form>
         <h3>Leaves in: </h3>
